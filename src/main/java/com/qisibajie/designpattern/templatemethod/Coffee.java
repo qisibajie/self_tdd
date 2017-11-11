afbf28d0640f4bf65920ddb000c4ee0482970d24
qisibajie <qisibajie@163.com>
2017-11-11 23:47:25 +0800

    [step1-add coffee and tea]

---
+++ src/main/java/com/qisibajie/designpattern/templatemethod/Coffee.java
@@ -0,0 +1,26 @@
+package com.qisibajie.designpattern.templatemethod;
+
+public class Coffee {
+    void prepareRecipe() {
+        boilWater();
+        brewCoffeeGrinds();
+        pourInCup();
+        addSugarAndMilk();
+    }
+
+    private void addSugarAndMilk() {
+        System.out.println("Add Sugar and Milk");
+    }
+
+    private void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    private void brewCoffeeGrinds() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    private void boilWater() {
+        System.out.println("Boiling water");
+    }
+}
---
+++ src/main/java/com/qisibajie/designpattern/templatemethod/Tea.java
@@ -0,0 +1,26 @@
+package com.qisibajie.designpattern.templatemethod;
+
+public class Tea {
+    void prepareRecipe() {
+        boilWater();
+        steepTeaBag();
+        pourInCup();
+        addLemon();
+    }
+
+    private void addLemon() {
+        System.out.println("Adding Lemon");
+    }
+
+    private void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    private void steepTeaBag() {
+        System.out.println("Steeping the tea");
+    }
+
+    private void boilWater() {
+        System.out.println("Boiling water");
+    }
+}
